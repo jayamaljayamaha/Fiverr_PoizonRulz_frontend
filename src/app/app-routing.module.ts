9c285654d8a6885343128c0176d725efd1b3fa90
Jayamal <jayamaljayamaha2@gmail.com>
2019-09-14 11:04:45 +0530

    added suitabale endpoint calls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ProjectDetailComponent } from './project-detail/project-detail.component';
-import { AppComponent } from './app.component';
-import { MainMenuComponent } from './main-menu/main-menu.component';
-import { HomeComponent } from './home/home.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ProjectDetailComponent} from './project-detail/project-detail.component';
+import {AppComponent} from './app.component';
+import {MainMenuComponent} from './main-menu/main-menu.component';
+import {HomeComponent} from './home/home.component';
+import {SideMenuComponent} from './side-menu/side-menu.component';
 
 
 const routes: Routes = [
-  
-  { path: '', component: MainMenuComponent},
-  { path: 'project-detail', component: ProjectDetailComponent}
+
+  {path: '', component: MainMenuComponent},
+  {path: 'project-detail', component: ProjectDetailComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import  axios  from 'axios';
+import {Component, OnInit} from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,34 +8,36 @@ import  axios  from 'axios';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { 
-    
+  constructor() {
+
   }
 
   ngOnInit() {
-   
+
   }
+
   enteredEmail = '';
   enteredPassword = '';
   confirmPassword = '';
 
-  login(){
-    axios.post('http://localhost:8000/login' , {userEmail: this.enteredEmail, userPassword: this.enteredPassword})
-    .then(function (response) {
-      console.log(response.data['message']);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  login() {
+    axios.post('http://localhost:8000/user/auth/login', {email: this.enteredEmail, password: this.enteredPassword})
+      .then(function (response) {
+        console.log(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
-  register(){
-    axios.post('http://localhost:8000/register' , {userEmail: this.enteredEmail, userPassword: this.enteredPassword})
-    .then(function (response) {
-      console.log(response.data['message']);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+
+  register() {
+    axios.post('http://localhost:8000/user/auth/register', {email: this.enteredEmail, password: this.enteredPassword})
+      .then(function (response) {
+        console.log(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
 }
