d387c93ba4891188890306eebb0a839b701b826c
yogeshadhikari <yogeshmike@gmail.com>
2019-09-27 00:17:38 +1000

    Updated Front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,15 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { from } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClientModule }    from '@angular/common/http';
+import { CallbackComponent } from './callback/callback.component';
+import { ExternalApiComponent } from './external-api/external-api.component';
 
 @NgModule({
   declarations: [
@@ -21,11 +26,16 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     ProfileComponent,
     TopMenuComponent,
-    LoginComponent
+    LoginComponent,
+    NavBarComponent,
+    CallbackComponent,
+    ExternalApiComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import  axios  from 'axios';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  enteredProjectName = '';
+  enteredSummary = '';
+  imageUpload = '';
+
+  newProject(){
+    axios.post('http://localhost:8000/newProject' , {projectName: this.enteredProjectName, projectSummary: this.enteredSummary, image: this.imageUpload})
+    .then(function (response) {
+      console.log(response.data['message']);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
 }
